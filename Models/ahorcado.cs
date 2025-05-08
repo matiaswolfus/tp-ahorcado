8c053865dd4bb34c2839cd0e680c13a4fc3e89dd
MATÍAS WOLFUS <49194355@ort.edu.ar>
2025-05-08 15:57:50 -0300

    arranque la idea, falta visualizar las cosas

---
+++ Models/ahorcado.cs
@@ -0,0 +1,19 @@
+namespace TP_AHORCADO.Models;
+
+public class ahorcado
+{
+    string palabra; 
+    char letraIngresada;
+    string palabraIngresada;
+    int intentos;
+
+
+
+//Foreach(char item in (mi palabra))
+//{
+//	if (view.Bag.letraIngresada == item)
+//	{
+//		mostrar la letra
+//	}
+}
+}
